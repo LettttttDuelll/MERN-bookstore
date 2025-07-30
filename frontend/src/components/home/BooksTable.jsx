50a31e7a7de82869c9e1f4753df940a92c6940a4
LettttttDuelll <phamdung07102004@gmail.com>
2025-07-30 10:24:31 +0700

    update _id

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -21,7 +21,7 @@
         </thead>
         <tbody>
           {books.map((book, index) => (
-            <tr key={book.id} className='h-8'>
+            <tr key={book._id} className='h-8'>
               <td className='border border-slate-700 rounded-md text-center'>
                 {index + 1}
               </td>
