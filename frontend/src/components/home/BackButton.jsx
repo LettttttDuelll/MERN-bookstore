1ec862997ede32b17bf7e78003d334863960f91a
LettttttDuelll <phamdung07102004@gmail.com>
2025-07-30 10:47:50 +0700

    update backButton

--- frontend/src/components/home/BackButton.jsx
+++ frontend/src/components/home/BackButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
 
-const BackButton = ({ destination = '/home'}) => {
+const BackButton = ({ destination = '/'}) => {
   return (
     <div className='flex'>
         <Link to={destination} className=''>
